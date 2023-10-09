a1a386ee1116541e906b242ee424298dea92e567
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2023-10-09 04:29:25 +0200

    Add method _read_bib_file()

--- bibliography.py
+++ bibliography.py
@@ -4,7 +4,9 @@ from abc import ABC
 class Bibliography(ABC):
     def __init__(self, filepath: str) -> None:
         self.filepath = filepath
+        self.bib_file_contents = None
 
     def _read_bib_file(self) -> None:
-        pass
-    
+        with open(self.filepath, 'r') as bib_file:
+            self.bib_file_contents = bib_file.read()
+        
